681b972afc20193d51c89cd987a3c1217be7babe
Md Shawon <shawonmohammad787@gmail.com>
2026-02-18 16:35:02 +0600

    cash out validation

---
+++ cashout.js
@@ -0,0 +1,40 @@
+document.getElementById("cashout-btn").addEventListener("click", function(){
+    // 1-get the agent number and validate
+    const cashoutNumberInput = document.getElementById("cashout-number");
+    const cashoutNumber = cashoutNumberInput.value;
+    console.log(cashoutNumber);
+    if(cashoutNumber.length !== 11){
+        alert("Invalid Number");
+        return;
+    }
+
+    // 2-get the amount and validate
+    const cashoutAmountInput = document.getElementById("cashout-amount");
+    const cashoutAmount = cashoutAmountInput.value;
+    console.log(cashoutAmount)
+
+    // 3-get the current balance
+    const balanceElement = document.getElementById("balance");
+    const balance = balanceElement.innerText;
+    console.log(balance)
+
+    // 4-calculate the new balance
+    const newBalance = Number(balance) - Number(cashoutAmount);
+    if(newBalance < 0){
+        alert("Invalid Amount");
+        return;
+    }
+
+    //  5-verify pin
+    const cashoutPinInput = document.getElementById("cashout-pin");
+    const pin = cashoutPinInput.value;
+    if(pin === '1234'){
+        alert("Cashout Succeed");
+        console.log(newBalance);
+        balanceElement.innerText = newBalance;
+    }
+    else{
+        alert("Invalid pin");
+        return;
+    }
+})
